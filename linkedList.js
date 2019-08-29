6dba56fe0976e02ecbafe174508e14957f49711b
Martha Plantz <maplantz89@gmail.com>
2019-08-29 13:56:20 -0700

    up to 5 done

--- linkedList.js
+++ linkedList.js
@@ -28,6 +28,59 @@ class LinkedList {
     }
   }
 
+  insertBefore(item, beforeNode){
+    if(this.head === null){
+      this.insertFirst(item);
+    }
+    if(beforeNode === this.head){
+      this.insertFirst(item);
+    }
+    let currNode = this.head;
+    let prevNode = this.head;
+  
+    let targetNode = this.find(beforeNode);
+    while(currNode !== targetNode){
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+    prevNode.next = new _Node(item, targetNode);
+    // console.log(prevNode);
+    // console.log(prevNode.next);
+  }
+
+  insertAfter(item, afterNode){
+    if(this.head === null){
+      this.insertFirst(item);
+    }
+    let currNode = this.head;
+    let targetNode = this.find(afterNode);
+
+    while(currNode !== targetNode){
+      currNode = currNode.next;
+    }
+    currNode.next = new _Node(item, targetNode.next);
+    // console.log(currNode);
+    // console.log(currNode.next);
+  }
+
+  insertAt(item, location){
+    if(this.head === null){
+      this.insertFirst(item);
+    }
+    if(location === 0){
+      this.insertFirst(item);
+    }
+    let currNode = this.head;
+    let prevNode = this.head;
+    for(let i=0; i<location; i++){
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+    prevNode.next = new _Node(item, currNode);
+    console.log(prevNode);
+    console.log(prevNode.next);
+  }  
+
   find(item){
     //start at the head
     let currNode = this.head;
@@ -78,5 +131,173 @@ class LinkedList {
     //making new connection of next pointer
     previousNode.next = currNode.next;
   }
-
 }
+
+function main(){
+  let SLL = new LinkedList;
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  //console.log(SLL.find('Starbuck'));
+  //SLL.insertLast('Tauhida');
+  //SLL.remove('Tauhida');
+  //console.log(SLL.find('Tauhida'));
+  //console.log(SLL.remove('squirrel'));
+  //console.log(SLL.insertBefore('New', 'Helo'));
+  //console.log(SLL.insertBefore('Test', 'Husker'));
+  //console.log(SLL.insertAfter('After', 'Boomer'));
+  //console.log(SLL.insertAt('At', 3));
+  //console.log(SLL.insertAt('At', 2));
+  //console.log(SLL.find('At'));
+  return SLL;
+}
+
+let list = main();
+
+function display(list){
+  if(list.head === null){
+    return 'empty linked list';
+  }
+  let display = [];
+  let currNode = list.head;
+  while(currNode !== null){
+    display.push(currNode);
+    currNode = currNode.next;
+  }
+  console.log(display);
+}
+//display(list);
+
+function size(list){
+  let count = 0;
+  if(list.head === null){
+    return 0;
+  }
+  let currNode = list.head;
+  while(currNode !== null){
+    count ++;
+    currNode = currNode.next;
+  }
+  console.log(count);
+  return count;
+}
+//size(list);
+
+function isEmpty(list){
+  if(list.head === null){
+    return 'Your Linked List is empty';
+  }
+  return 'Your linked list has items';
+}
+// console.log(isEmpty(list));
+
+let empty = new LinkedList();
+// console.log(isEmpty(empty));
+
+function findPreviousByValue(list, value){
+  let currNode = list.head;
+  let prevNode = list.head;
+
+  for(let i=0; i<value; i++){
+    currNode = currNode.next;
+    prevNode = currNode;
+  }
+  return(prevNode);
+}
+
+findPreviousByValue(list, 2);
+//check that correct item, linkedlist starts 0, 1, 2, ...
+
+function findPrevious(list, item){
+  if(item === list.head.value){
+    return 'No item before';
+  }
+  let currNode = list.head;
+  let prevNode = list.head;
+  while(currNode.value !== item){
+    prevNode = currNode;
+    currNode = currNode.next;
+    if(currNode.next === null && currNode.value !== item){
+      console.log('Item not found');
+      return 'Item not found';
+    }
+  }
+  console.log(prevNode);
+  return prevNode;
+}
+
+// findPrevious(list, 'Helo');
+// findPrevious(list, 'Boomer');
+// findPrevious(list, 'Starbuck');
+// findPrevious(list, 'sdf');
+// findPrevious(list, 'Apollo');
+
+function findLast(list){
+  if(list.head === null){
+    return 'No items';
+  }
+  let currNode = list.head;
+  while(currNode.next !== null){
+    currNode = currNode.next;
+  }
+  console.log(currNode);
+  return currNode;
+}
+
+//findLast(list);
+
+
+//4. 
+function WhatDoesThisProgramDo(lst) {
+  let current = lst.head;
+  while (current !== null) {
+    let newNode = current;
+    while (newNode.next !== null) {
+      if (newNode.next.value === current.value) {
+        newNode.next = newNode.next.next;
+      }
+      else {
+        newNode = newNode.next;
+      }
+    }
+    current = current.next;
+  }
+}
+/**
+ *  A, B, B, C
+ *  head = A <- newNode while  A is not last
+ *  if B
+ * 
+ *  This function deletes any dupliate values from the linked list 
+ *  it does this by reassigning the next pointer if it comes across any 
+ *  duplicate values. 
+ *  O(n^2) - nested loop
+ *  
+ */
+
+
+function reverse(list) {
+  if(list.head === null){
+    console.log('you have an empty linked list');
+    return 'You have an empty linked list';
+  }
+  let currNode = list.head;
+  let prevNode = null;
+  let nextNode = list.head;
+  while(currNode !== null){
+    nextNode = currNode.next;
+    currNode.next = prevNode;
+    prevNode = currNode;
+    currNode = nextNode;
+  }
+  list.head = prevNode;
+  display(list);
+}
+reverse(list);
+
+// a -> b -> c
+// a <- b <- c
+// new Node(item, prevNode)
